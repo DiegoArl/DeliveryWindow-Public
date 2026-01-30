744c678f6c7357da2bfa58d29e25eddee448e9e8
DiegoArl <166954500+DiegoArl@users.noreply.github.com>
2026-01-30 15:00:48 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- procesamiento.py
+++ procesamiento.py
@@ -327,8 +327,8 @@ def agregar_equipo(df, df_usuarios, mapa_equipo, adopcion=False):
     else:
         df = df.merge(
             usuarios[["Rep. Ventas", "Equipo"]],
-            left on="nombrevendedor",
-            right on= "Rep. Ventas",
+            left_on="nombrevendedor",
+            right_on= "Rep. Ventas",
             how="left"
         ).drop(columns="Rep. Ventas")
     
@@ -547,6 +547,7 @@ def AdopcionVendedores(df_users, mapa_equipo, width=1000, height=550):
     fig = crear_tabla_adopcion(df_procesado, width=width, height=height)
     fig.show()
     return df_procesado
+
 
 
 
